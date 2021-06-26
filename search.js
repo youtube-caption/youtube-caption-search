8031b49e6777998981f9ef471010d0cb4a1c3a36
Yunbin Chang <yunbin@hansung.ac.kr>
2021-06-26 14:00:59 +0900

    Link once to shortcut setting page on the chrome when it is installed

--- search.js
+++ search.js
@@ -1,2 +1,11 @@
-import { SearchEngine } from './search_engine.js';
+import { SearchEngine } from "./search_engine.js";
+
+// First Install
+if (!window.localStorage.getItem("hasSeenIntro")) {
+  window.localStorage.setItem("hasSeenIntro", "yep");
+  chrome.tabs.create({
+    url: "chrome://extensions/shortcuts",
+  });
+}
+
 new SearchEngine();
